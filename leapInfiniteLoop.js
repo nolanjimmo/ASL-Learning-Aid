486c4c23d62d307f71a26bf52aecc91c19a102fb
nolanjimmo <barcafan12345@gmail.com>
2020-09-03 10:21:49 -0400

    Got deliverable 1, step 52 working

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,8 +1,13 @@
 var controllerOptions ={};
 var x = window.innerWidth / 2;
 var y = window.innerHeight / 2;
+var deltaX;
+var deltaY;
 Leap.loop(controllerOptions, function(frame)
 {
-    circle(x,y,50);
+    clear();
+    deltaX = Math.round(Math.random()) * 2 - 1;
+    deltaY = Math.round(Math.random()) * 2 - 1;
+    circle(x + deltaX,y+deltaY,50);
 }
 );
