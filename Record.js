b22008f8e73e71615e9d19311f6658981f0c21e7
nolanjimmo <barcafan12345@gmail.com>
2020-09-21 09:46:52 -0400

    Deliverable 3, step 3, run the Record.html file successfully

--- Record.js
+++ Record.js
@@ -6,8 +6,10 @@ var rawXMin = 500;
 var rawXMax = -500;
 var rawYMin = 500;
 var rawYMax = -500;
-var screenX;
-var screenY;
+var screenX1;
+var screenY1;
+var screenX2;
+var screenY2;
 var oldXRange;
 var oldYRange;
 var newXRange = window.innerWidth;
@@ -32,40 +34,57 @@ function HandleFrame(frame){
 
 function HandleHand(hand){
     fingers = hand.fingers;
-        for (var f=0; f<fingers.length; f++)
-        {
-            if(fingers[f].type == 1){
-                HandleFinger(fingers[f]);
+        for (var f=fingers.length - 1; f>=0; f--) {
+            for (var b=fingers[f].bones.length - 1; b >= 0; b--)  {
+                HandleBone(fingers[f].bones[b]);
             }
-        }
+        };
+};
+
+function HandleBone(bone){
+    var bone_start = bone.prevJoint;
+    var bone_end = bone.nextJoint;
+
+    [screenX1, screenY1] = TransformCoordinates(bone_start[0], bone_start[1]);
+    [screenX2, screenY2] = TransformCoordinates(bone_end[0], bone_end[1]);
+
+
+    //If anyone comes to look at how I did the strokeWeight calculation... I don't really understand why you would need to pass another param
+    //in to this function when you have the bone type already accessible, so I just this visual conversion using the bone type... I don't really
+    //even see another useful variable within finger that isn't within the bone.
+    strokeWeight((10 - (3 * bone.type)));
+    //stroke((bone.type * 5) * 2);
+    stroke(180 - bone.type * 60);
+    line(screenX1 - window.innerWidth, window.innerHeight - screenY1, screenX2 - window.innerWidth, window.innerHeight - screenY2);
+
 };
 
 function HandleFinger(finger){
-    //console.log(finger);
+    console.log(finger);
     var tipPos = finger.tipPosition;
-    //console.log(tipPos);
-    if(tipPos[0] < rawXMin){
-        rawXMin = tipPos[0];
-    }
-    if(tipPos[0] > rawXMax){
-        rawXMax = tipPos[0];
-    }
-    if(tipPos[1] < rawYMin){
-        rawYMin = tipPos[1];
-    }
-    if(tipPos[1] > rawYMax){
-        rawYMax = tipPos[1];
+
+    for (var b=0; b<finger.bones.length; ++b){
+        HandleBone(finger.bones[b]);
     }
 
-    screenX = (((tipPos[0] - rawXMin) * newXRange) / oldXRange) + 0;
-    screenY = (((tipPos[1] - rawYMin) * newYRange) / oldYRange) + 0;
-
-    circle(screenX, (window.innerHeight-screenY), 50);
-
-    //z = finger.tipPosition[2];
-    //circle(x+(.5*window.innerWidth),(window.innerHeight-y)-(.5*window.innerHeight),50);
-    //console.log(rawXMin);
-    //console.log(rawXMax);
-    //console.log(rawYMin);
-    //console.log(rawYMax);
 }
+
+function TransformCoordinates (x,y) {
+    if(x < rawXMin){
+        rawXMin = x;
+    }
+    if(x > rawXMax){
+        rawXMax = x;
+    }
+    if(y < rawYMin){
+        rawYMin = y;
+    }
+    if(y > rawYMax){
+        rawYMax = y;
+    }
+
+    x = (((x - rawXMin) * newXRange) / oldXRange) + 0;
+    y = (((y - rawYMin) * newYRange) / oldYRange) + 0;
+
+    return [x,y];
+};
